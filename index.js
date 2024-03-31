7a8eeb0e12d1a2a307307ad70ee4f049709ec0fb
David <Muchahi 853@gmail.com>
2024-03-31 20:08:51 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names, gift){
+    return names.map(name => `Thank you, ${names}, for the wonderful ${gift}!`);
+}
+
+  // Function to count down from a given number
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
